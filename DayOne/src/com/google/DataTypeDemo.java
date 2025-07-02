e48d28f71132581ced636647d348f5fc78d57a3b
sakshi <sakshi@LAPTOP-9JFL93DO>
2025-07-02 21:53:26 +0530

    variable

---
+++ DayOne/src/com/google/DataTypeDemo.java
@@ -0,0 +1,41 @@
+package com.google;
+
+public class DataTypeDemo {
+	
+	 public static void main(String[] args) {
+		 
+		 //byte
+		  byte byteMax = 127;
+		  byte byteMin = -128;
+		  
+		  System.out.println(byteMax);
+		  System.out.println(byteMin);
+		  
+		  //short
+		   short shortMax = 32767;
+		   short shortMin = -32768;
+		   
+		   System.out.println("Min range of short is"+ shortMin + "Max range of short is"+ shortMax);
+		   
+		   //int
+		    int MaxInt = 214657;
+		    int MinInt = -3236656;
+		    
+		   //long
+		    long maxLong = 336565667637656747L;
+		    long minLong = -62355454527778L;
+		    
+		    
+		    float f = 4354.5654635f;
+		    
+		    double d = 54546.65673656546736d;
+		    
+		    
+		    //boolean
+		    
+		    boolean bool = false;
+		    System.out.println(bool);
+		    
+		    }
+
+}
---
+++ DayOne/src/com/google/helloworld.java
@@ -0,0 +1,9 @@
+package com.google;
+
+public class helloworld {
+      
+	 public static void main(String[] args) {
+		 System.out.print(" first program");
+	 }
+	 
+}
